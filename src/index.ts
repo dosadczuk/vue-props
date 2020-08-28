154974fef94f5dc49e210049198e99fa294a0a23
dosadczuk <damian.osadczuk@gmail.com>
2020-08-28 15:18:25 +0200

    Change index exports

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
 // @ts-ignore
 import { PropOptions, Nullable } from './props';
-import * as PropArray from './props/array';
-import * as PropBoolean from './props/boolean';
-import * as PropDate from './props/date';
-import * as PropFunction from './props/function';
-import * as PropNumber from './props/number';
-import * as PropObject from './props/object';
-import * as PropString from './props/string';
+import { RequiredArray, DefaultArray } from './props/array';
+import { RequiredBoolean, DefaultBoolean } from './props/boolean';
+import { RequiredDate, DefaultDate } from './props/date';
+import { RequiredFunction, DefaultFunction } from './props/function';
+import { RequiredNumber, DefaultNumber } from './props/number';
+import { RequiredObject, DefaultObject } from './props/object';
+import { RequiredString, DefaultString } from './props/string';
 
-export const RequiredString = PropString.RequiredString;
-export const DefaultString = PropString.DefaultString;
+export default {
+    RequiredArray, DefaultArray,
+    RequiredBoolean, DefaultBoolean,
+    RequiredDate, DefaultDate,
+    RequiredFunction, DefaultFunction,
+    RequiredNumber, DefaultNumber,
+    RequiredObject, DefaultObject,
+    RequiredString, DefaultString,
+};
 
-export const RequiredNumber = PropNumber.RequiredNumber;
-export const DefaultNumber = PropNumber.DefaultNumber;
-
-export const RequiredBoolean = PropBoolean.RequiredBoolean;
-export const DefaultBoolean = PropBoolean.DefaultBoolean;
-
-export const RequiredDate = PropDate.RequiredDate;
-export const DefaultDate = PropDate.DefaultDate;
-
-export const RequiredArray = PropArray.RequiredArray;
-export const DefaultArray = PropArray.DefaultArray;
-
-export const RequiredObject = PropObject.RequiredObject;
-export const DefaultObject = PropObject.DefaultObject;
-
-export const RequiredFunction = PropFunction.RequiredFunction;
-export const DefaultFunction = PropFunction.DefaultFunction;
+export {
+    RequiredArray, DefaultArray,
+    RequiredBoolean, DefaultBoolean,
+    RequiredDate, DefaultDate,
+    RequiredFunction, DefaultFunction,
+    RequiredNumber, DefaultNumber,
+    RequiredObject, DefaultObject,
+    RequiredString, DefaultString,
+};
