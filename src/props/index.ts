e34ac5e5d2423af464f185391af87ec961181f70
dosadczuk <damian.osadczuk@gmail.com>
2020-08-28 22:20:50 +0200

    Changes for Vue3

--- src/props/index.ts
+++ src/props/index.ts
@@ -1,10 +1,16 @@
-import { PropOptions as VuePropOptions, PropType as VuePropType } from 'vue/types/options';
+import { PropType as VuePropType } from 'vue';
 
 export type Nullable<T> = T | null;
-export type PropOptions<T> = VuePropOptions<T>;
 
 type PropType<T> = VuePropType<T>;
 type PropDefault<T> = Nullable<T> | NonNullable<() => T>;
+
+export interface PropOptions<T, D = T> {
+  type?: PropType<T>;
+  required?: boolean;
+  default?: PropDefault<D>;
+  validator?(value: T): boolean;
+}
 
 export const Prop = <T>(type: PropType<T>, o?: PropOptions<T>): PropOptions<T> => ({ type, ...o });
 export const Required = <T>(t: PropType<T>, o?: PropOptions<T>): PropOptions<T> => Prop<T>(t, { required: true, ...o });
